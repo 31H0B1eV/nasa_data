6ce76eaa06a8be05ddfbf660a368e0270b060882
Artem Zinoviev <arzinoviev@gmail.com>
2014-12-30 18:37:44 +0300

    new animation

--- index.php
+++ index.php
@@ -6,30 +6,44 @@ error_reporting(-1);
 
 getImageArray();
 
-$im = new Imagick();
-$im->setFormat('GIF');
-
-$frameCount = 0;
 $files =  glob('img/*.jpeg');
 var_dump($files);
 
-foreach($files as $pic) {
-    if($pic == "." || $pic == "..") continue;
-    $frame = new Imagick($pic);
-    $frame->thumbnailImage(600, 600);
-    $im->addImage($frame);
-    $im->setImageDelay((($frameCount % 11) * 5));
-    $im->nextImage();
+/**
+ * @param $file_name array of files
+ * @param $date array with index 0 => as startDate and 1 => as endDate
+ * @throws ErrorException
+ */
+function createGif($file_name, $date)
+{
+    if(!is_array($date) || !is_array($file_name)) {
+        throw new ErrorException('createGif function parameters must be arrays');
+    } else {
 
-    $frameCount++;
+        $im = new Imagick();
+        $im->setFormat('GIF');
+
+        $frameCount = 0;
+
+        foreach($file_name as $pic) {
+            if($pic == "." || $pic == "..") continue;
+            $frame = new Imagick($pic);
+            $frame->thumbnailImage(600, 600);
+            $im->addImage($frame);
+            $im->setImageDelay((($frameCount % 11) * 5));
+            $im->nextImage();
+
+            $frameCount++;
+        }
+
+        $im->writeImages('img.gif', true);
+    }
 }
-
-$im->writeImages('img.gif', true);
 
 function getImageArray()
 {
     $images = array();
-    $timestamp = '2014-12-30%2000:00:00.0';
+    $timestamp = '2014-12-31%2000:00:00.0';
     $num = '20';
     $url = 'http://iswa.gsfc.nasa.gov/IswaSystemWebApp/CygnetLastNInstancesServlet?cygnetId=251&endTimestamp='. $timestamp . '&lastN=' . $num;
 
@@ -51,9 +65,13 @@ function saveImage($img)
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response= curl_exec ($ch);
         curl_close($ch);
-        $file_name = "./img/".substr($k, 0, -3).".jpeg"; // .".jpeg"
-        $file = fopen($file_name , 'w') or die("X_x");
-        fwrite($file, $response);
-        fclose($file);
+        $file_name = "./img/".substr($k, 0, -3).".jpeg";
+        if(!file_exists($file_name)) {
+            $file = fopen($file_name , 'w') or die("X_x");
+            fwrite($file, $response);
+            fclose($file);
+        } else {
+            file_put_contents('log', 'File: ' . $file_name . ' exists' . "\n", FILE_APPEND);
+        }
     }
 }
